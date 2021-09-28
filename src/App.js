f0ce7dc06f708a9695775f55048708e5606b4cbb
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-09-28 06:41:36 -0400

    Need to do anonSnapshot

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from 'react';
+
+// MATERIAL UI
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
+// import DateTimePicker from '@mui/lab/DateTimePicker';
+// import EventIcon from '@mui/icons-material/Event';
+// import Input from '@mui/material/Input';
+// import InputAdornment from '@mui/material/InputAdornment';
+
+// FIREBASE
+import { collection, addDoc, serverTimestamp, onSnapshot, getDoc } from 'firebase/firestore';
 import db from './firebaseConfig';
 
+// CSS
 import './App.css';
 
 function App() {
+
+
  const [todoInput, setTodoInput] = useState('');
- const [date, setDate] = useState('');
+ // const [date, setDate] = useState('');
+
+ useEffect(() => {
+  getTodos();
+ }, []);
+
+ const getTodos = async () => {
+  const docRef = collection(db, "todos");
+  const retrieveDoc = await getDoc(docRef);
+ };
 
 
  const addTodo = async (e) => {
@@ -20,10 +39,12 @@ function App() {
   const payload = {
    inProgress: true,
    todo: todoInput,
-   deadlineDate: date,
+   // deadlineDate: date,
    timestamp: serverTimestamp()
   };
   await addDoc(collectionRef, payload);
+  setTodoInput('');
+  // setDate('');
  };
 
  return (
@@ -39,22 +60,22 @@ function App() {
      value={todoInput}
      onChange={(e) => setTodoInput(e.target.value)}
     />
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
      <DateTimePicker
       renderInput={(props) => <TextField {...props}
-       className="todo-app__text-field"
-       id="standard-basic"
+       // className="todo-app__text-field"
+       // id="standard-basic"
        label="Choose a deadline"
        variant="standard"
-       onChange={(e) => setTodoInput(e.target.value)} />}
+      />}
       label=""
       value={date}
       onChange={(newValue) => {
        setDate(newValue);
       }}
      />
-    </LocalizationProvider>
-    <Button type='submit' variant="contained" onClick={addTodo}>Default</Button>
+    </LocalizationProvider> */}
+    <Button type='submit' variant="contained" onClick={addTodo} style={{ 'display': 'none' }}>Default</Button>
    </form >
 
   </div >
